c53041fb039599ffd83666394fa8112b6d83be32
e <3520845@qq.com>
2017-04-26 20:29:18 +0800

    11

--- sskl_webui/templates/pages/S_Station_List.js
+++ sskl_webui/templates/pages/S_Station_List.js
@@ -1,7 +1,55 @@
 		<script type="text/javascript">
 			$(document).ready(function() {
+
 				$('#example').DataTable( {
-					"ajax": "/assets/sskl_webui/arrays.txt"
+					"dom": 'lfrtp',
+					//"bInfo" : false,
+					//"pagingType": "full_numbers" ,
+					"ajax": {
+						"url": "/api/method/tieta.tieta.doctype.cell_station.cell_station.search_station?rgn=RGN000005",
+						// "url": "/assets/sskl_webui/arrays.txt",
+						"dataSrc":"message",
+                    },
+					"columns": [
+										{ "data": "code" },
+										{ "data": "station_name" },
+										{ "data": "station_type" },
+										{ "data": "address_text" },
+
+
+									]
+
+
 				} );
+
+				var table = $('#example').DataTable();
+				    $('#example tbody').on('click', 'tr', function () {
+						var data = table.row( this ).data();
+						console.log(data['name']);
+						//alert( 'You clicked on '+data[0]+'\'s row' );
+
+
+					} );
+
+					if ((screen.width<=500) && (screen.height<=800)) {
+						console.log('移动设备：',screen.width,'*',screen.height);
+/*				$('#example').DataTable( {
+					bLengthChange: false,
+					    "language": {
+                            "info": "",
+                            "paginate": {
+                                "previous": "<<",
+                                "next": ">>"
+                            },
+                        }
+				});*/
+						//table.column(2).visible(false);
+						//table.column(3).visible(false);
+					} else {
+						console.log('桌面设备：',screen.width,'*',screen.height);
+
+					}
+
+
 			} );
 		</script>
